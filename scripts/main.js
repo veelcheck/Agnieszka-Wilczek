a77c718c06c5018eef9ac233a96db22f66cccdfc
veelcheck <aga_wilczek@onet.eu>
2023-10-19 13:38:00 +0200

    drop menu smooth slide

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,20 @@ import { currentTime } from "./clock.js";
 
 const navToggle = document.querySelector('.nav__toggle');
 const nav = document.querySelector('.nav');
+const containerNav = document.querySelector('.container-nav');
 
 navToggle.addEventListener('click', () => {
   nav.hasAttribute('data-visible')
     ? navToggle.setAttribute('aria-expanded', false)
     : navToggle.setAttribute('aria-expanded', true);
   nav.toggleAttribute('data-visible');
+
+  setTimeout(() => {
+    containerNav.toggleAttribute('data-visible');
+  }, 200);
+
+  setTimeout();
+  
 });
 
 currentTime();
